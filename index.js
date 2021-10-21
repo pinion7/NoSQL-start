ce2823636a64f83c3f9343b8652797a39bed7ef9
박민수 <pinion7rl@gmail.com>
2021-10-21 13:47:07 +0900

    express, nodemon 세팅

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+  return res.send("hello world");
+})
+
+app.listen(3000, () => {
+  console.log("서버 가동");
+})
